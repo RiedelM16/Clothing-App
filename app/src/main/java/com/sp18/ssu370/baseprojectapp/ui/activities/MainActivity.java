1ce5e164644ead0aba7d4032ef2001d210901706
gabriel <duartega@sonoma.edu>
2018-03-20 16:50:06 -0700

    Update on Designs. More to come. 3/20 4:50pm -Gabe

--- app/src/main/java/com/sp18/ssu370/baseprojectapp/ui/activities/MainActivity.java
+++ app/src/main/java/com/sp18/ssu370/baseprojectapp/ui/activities/MainActivity.java
@@ -2,6 +2,10 @@ package com.sp18.ssu370.baseprojectapp.ui.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
 
 import com.sp18.ssu370.baseprojectapp.R;
 
@@ -11,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button ClosetBtns = (Button) findViewById(R.id.ClosetBtn);
+        ClosetBtns.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                setContentView(R.layout.layout);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/sp18/ssu370/baseprojectapp/ui/activities/layoutClass.java
@@ -0,0 +1,24 @@
+package com.sp18.ssu370.baseprojectapp.ui.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.sp18.ssu370.baseprojectapp.R;
+
+/**
+ * Created by Gabri on 3/20/2018.
+ */
+
+public class layoutClass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
