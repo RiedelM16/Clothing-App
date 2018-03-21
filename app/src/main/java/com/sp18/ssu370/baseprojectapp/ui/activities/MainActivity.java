674a737290d71eb4c1593f8b4b53fac49a798baf
gabriel <duartega@sonoma.edu>
2018-03-20 23:34:19 -0700

    Added Sliding Mechanism. It is just colors as of now. 3/20 11:33pm -Gabe

--- app/src/main/java/com/sp18/ssu370/baseprojectapp/ui/activities/MainActivity.java
+++ app/src/main/java/com/sp18/ssu370/baseprojectapp/ui/activities/MainActivity.java
@@ -1,11 +1,10 @@
 package com.sp18.ssu370.baseprojectapp.ui.activities;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.content.Intent;
-
 
 import com.sp18.ssu370.baseprojectapp.R;
 
@@ -16,7 +15,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button ClosetBtn = (Button) findViewById(R.id.ClosetBtn);
+        ViewPager viewPager = findViewById(R.id.ScrollViewPgr);
+        viewPager.setAdapter(new CustomPagerAdapter(this));
+
+        Button ClosetBtn = findViewById(R.id.ClosetBtn);
         ClosetBtn.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
--- app/src/main/java/com/sp18/ssu370/baseprojectapp/ui/activities/layoutClass.java
+++ app/src/main/java/com/sp18/ssu370/baseprojectapp/ui/activities/layoutClass.java
@@ -1,10 +1,7 @@
 package com.sp18.ssu370.baseprojectapp.ui.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 
 import com.sp18.ssu370.baseprojectapp.R;
 
