8c777f66a10b859eb9b1f1878eb8e4a93640153b
gabriel <duartega@sonoma.edu>
2018-05-01 15:05:25 -0700

    Got weather installed based on location 5/1 3:06pm -Gabe

--- app/src/main/java/com/sp18/ssu370/baseprojectapp/ui/activities/MainActivity.java
+++ app/src/main/java/com/sp18/ssu370/baseprojectapp/ui/activities/MainActivity.java
@@ -12,6 +12,34 @@ import android.graphics.Typeface;
 import android.text.Html;
 import android.widget.TextView;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.content.SharedPreferences;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.location.*;
+import android.location.LocationManager;
+import android.graphics.Typeface;
+import android.text.Html;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.support.v7.app.AlertDialog;
+import android.content.DialogInterface;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 
 
 import com.sp18.ssu370.baseprojectapp.R;
@@ -28,6 +56,122 @@ public class MainActivity extends AppCompatActivity {
 
     Typeface weatherFont;
     SharedPreferences prefs = null;
+    LocationManager locationManager;
+    String provider;
+
+
+    private FusedLocationProviderClient mFusedLocationClient;
+
+
+
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+
+
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                new AlertDialog.Builder(this)
+                        .setTitle(R.string.title_location_permission)
+                        .setMessage(R.string.text_location_permission)
+                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(MainActivity.this,
+                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                        MY_PERMISSIONS_REQUEST_LOCATION);
+                            }
+                        })
+                        .create()
+                        .show();
+
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // location-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+
+                        final Function.placeIdTask asyncTask = new Function.placeIdTask(new Function.AsyncResponse() {
+                            public void processFinish(String weather_city,
+                                                      String weather_description,
+                                                      String weather_temperature,
+                                                      String weather_humidity,
+                                                      String weather_pressure,
+                                                      String weather_updatedOn,
+                                                      String weather_iconText,
+                                                      String sun_rise) {
+
+                                cityField.setText(weather_city);
+
+                                detailsField.setText(weather_description);
+                                currentTemperatureField.setText(weather_temperature);
+                                weatherIcon.setText(Html.fromHtml(weather_iconText));
+                            }
+                        });
+                        //Request location updates:
+                        mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                            @Override
+                            public void onSuccess(Location location) {
+                                if (location != null) {
+                                    double lat = location.getLatitude();
+                                    double longi = location.getLongitude();
+                                    String latitude = String.valueOf(lat);
+                                    String longitude = String.valueOf(longi);
+
+
+                                    asyncTask.execute(latitude, longitude);
+
+                                }
+                                else
+                                    asyncTask.execute("25.180000", "89.530000"); //  asyncTask.execute("Latitude", "Longitude")
+                            }
+                        });
+
+                    }
+
+                } else {
+                    finish();
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+
+                }
+                return;
+            }
+
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +179,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         prefs = getSharedPreferences("com.mycompany.OutfitMatcher", MODE_PRIVATE);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        provider = locationManager.getBestProvider(new Criteria(), false);
+        checkLocationPermission();
 
         weatherFont = Typeface.createFromAsset(getApplicationContext().getAssets(), "fonts/weathericons-regular-webfont.ttf");
 
@@ -47,26 +196,6 @@ public class MainActivity extends AppCompatActivity {
         weatherIcon = (TextView)findViewById(R.id.weather_icon);
         weatherIcon.setTypeface(weatherFont);
 
-        Function.placeIdTask asyncTask =new Function.placeIdTask(new Function.AsyncResponse() {
-            public void processFinish(String weather_city,
-                                      String weather_description,
-                                      String weather_temperature,
-                                      String weather_humidity,
-                                      String weather_pressure,
-                                      String weather_updatedOn,
-                                      String weather_iconText,
-                                      String sun_rise) {
-
-                cityField.setText(weather_city);
-               // updatedField.setText(weather_updatedOn);
-                detailsField.setText(weather_description);
-                currentTemperatureField.setText(weather_temperature);
-               // humidity_field.setText("Humidity: "+weather_humidity);
-               // pressure_field.setText("Pressure: "+weather_pressure);
-                weatherIcon.setText(Html.fromHtml(weather_iconText));
-            }
-        });
-        asyncTask.execute("38.340329", "-122.699980"); //  asyncTask.execute("Latitude", "Longitude")
 
 
         ImageButton Camera = findViewById(R.id.CameraBtn);
@@ -98,6 +227,12 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //asyncTask.cancel(true);
+    }
+
     protected void onResume() {
         super.onResume();
 
@@ -109,5 +244,43 @@ public class MainActivity extends AppCompatActivity {
             // using the following line to edit/commit prefs
             prefs.edit().putBoolean("firstrun", false).commit();
         }
-    }
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            final Function.placeIdTask asyncTask = new Function.placeIdTask(new Function.AsyncResponse() {
+                public void processFinish(String weather_city,
+                                          String weather_description,
+                                          String weather_temperature,
+                                          String weather_humidity,
+                                          String weather_pressure,
+                                          String weather_updatedOn,
+                                          String weather_iconText,
+                                          String sun_rise) {
+
+                    cityField.setText(weather_city);
+
+                    detailsField.setText(weather_description);
+                    currentTemperatureField.setText(weather_temperature);
+                    weatherIcon.setText(Html.fromHtml(weather_iconText));
+                }
+            });
+            //Request location updates:
+            mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null) {
+                        double lat = location.getLatitude();
+                        double longi = location.getLongitude();
+                        String latitude = String.valueOf(lat);
+                        String longitude = String.valueOf(longi);
+
+                        asyncTask.execute(latitude, longitude);
+
+                    } else
+                        asyncTask.execute("25.180000", "89.530000"); //  asyncTask.execute("Latitude", "Longitude")
+                }
+            });
+        }
+
+        }
 }
